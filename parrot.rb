e4b6aa9f9b6fc01b66c06adf817ebba502e62a11
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-04 22:15:21 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot = ("Pretty Bird!")
+def parrot
+  phrase = "Pretty Bird!"
   puts "Squawk!"
   "Squawk!"
-
+  "Pretty bird!"
 end
-parrot
